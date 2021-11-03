a927e0ae67db8555f83548dbcb8be3343f10473b
NicolasGDas <50321430+NicolasGDas@users.noreply.github.com>
2021-11-03 12:50:02 -0300

    Cambio del adapter para utilizar un url para la imagen en lugar de hardcode en el repo

--- app/src/main/java/com/ort/usanote/adapters/ProductAdapter.kt
+++ app/src/main/java/com/ort/usanote/adapters/ProductAdapter.kt
@@ -1,5 +1,6 @@
 package com.ort.usanote.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,29 +8,48 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.ort.usanote.R
-import com.ort.usanote.entities.ProductForListProducts
+import com.ort.usanote.entities.Product
 
 class ProductAdapter(
-    private val productList:ArrayList<ProductForListProducts>,
+    private var productList : MutableList<Product>,
+    private val context: Context,
     val onClick:(Int)->Unit):RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
 
-        inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+        inner class ViewHolder(itemView: View, parentView:ViewGroup):RecyclerView.ViewHolder(itemView){
+
+            private var view : View
+            private var parent : ViewGroup
+
+            init {
+                this.view = itemView
+                this.parent = parentView
+            }
             var card : CardView = itemView.findViewById(R.id.card_view)
-            var itemImage: ImageView = itemView.findViewById(R.id.item_image)
             var itemTItle: TextView = itemView.findViewById(R.id.item_title)
             var itemDetail: TextView = itemView.findViewById(R.id.item_desc)
             var itemPrice: TextView = itemView.findViewById(R.id.item_price)
+            fun setImage(context: Context, imageUrl: String) {
+                var imgProductItem : ImageView = view.findViewById(R.id.item_image)
+                Glide
+                    .with(context)
+                    .load(imageUrl)
+                    .centerInside()
+                    .into(imgProductItem)
+            }
         }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.product_card_in_list_products,parent,false)
-        return ViewHolder(view)
+        return ViewHolder(view,parent)
     }
     override fun onBindViewHolder(holder: ViewHolder, i: Int) {
         holder.itemTItle.text = productList[i].title
         holder.itemDetail.text = productList[i].description
-        holder.itemPrice.text = productList[i].price
-        holder.itemImage.setImageResource(productList[i].imagen)
+        holder.itemPrice.text = productList[i].price.toString()
+        holder.setImage(context,productList[i].imageUrl)
         holder.card.setOnClickListener() {
             onClick(i)
         }
