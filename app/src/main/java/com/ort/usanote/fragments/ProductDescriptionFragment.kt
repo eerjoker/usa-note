8faf1739dbbe21c468f7daa3744ecd621e2d3b1d
NicolasGDas <50321430+NicolasGDas@users.noreply.github.com>
2021-11-03 12:48:43 -0300

    Agregado de funcionalidad al boton agregar al carrito y cambio de como se setea la imagen del producto

--- app/src/main/java/com/ort/usanote/fragments/ProductDescriptionFragment.kt
+++ app/src/main/java/com/ort/usanote/fragments/ProductDescriptionFragment.kt
@@ -8,7 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.navigation.findNavController
+import com.bumptech.glide.Glide
 import com.ort.usanote.R
+import com.ort.usanote.entities.Product
+import com.ort.usanote.entities.ProductItemRepository
 
 class ProductDescriptionFragment : Fragment() {
     lateinit var v : View
@@ -18,6 +21,8 @@ class ProductDescriptionFragment : Fragment() {
 
     private lateinit var viewModel: ProductDescriptionViewModel
     private lateinit var btnToCart:Button
+    private lateinit var btnAddCart: Button
+    private lateinit var itemsCarrito : ProductItemRepository
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +30,7 @@ class ProductDescriptionFragment : Fragment() {
     ): View? {
         v = inflater.inflate(R.layout.product_description_fragment, container, false)
         btnToCartInit(v)
+        btnAddCart(v)
         return v
     }
 
@@ -32,10 +38,23 @@ class ProductDescriptionFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProductDescriptionViewModel::class.java)
     }
+    private fun btnAddCart(v:View){
+        btnAddCart = v.findViewById(R.id.add_cart)
+        btnAddCart.setOnClickListener{
+            val spinner = v.findViewById<Spinner>(R.id.spinner_cantidad)
+            val title = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).title
+            val desc = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).description
+            val price = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).price
+            val image = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).image
+            val stock = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).cantidad
+            itemsCarrito = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).itemsCarrito
+            itemsCarrito.addProductItem(Product(title,desc,price,stock,image), spinner.selectedItem as Int)
+        }
+    }
     private fun btnToCartInit(v:View){
         btnToCart = v.findViewById(R.id.comprar)
         btnToCart.setOnClickListener{
-            val action = ProductDescriptionFragmentDirections.actionProductDescriptionFragmentToCarritoFragment()
+            val action = ProductDescriptionFragmentDirections.actionProductDescriptionFragmentToCarritoFragment(itemsCarrito)
             v.findNavController().navigate(action)
         }
     }
@@ -43,7 +62,6 @@ class ProductDescriptionFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
-        var imagen = v.findViewById<ImageView>(R.id.item_image)
         var nombre_producto = v.findViewById<TextView>(R.id.item_title)
         var desc = v.findViewById<TextView>(R.id.item_desc)
         var price = v.findViewById<TextView>(R.id.item_price)
@@ -56,7 +74,12 @@ class ProductDescriptionFragment : Fragment() {
         val adaptador = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item,array)
         spinner.adapter = adaptador
 
-        imagen.setImageResource(ProductDescriptionFragmentArgs.fromBundle(requireArguments()).image)
+        var imgProductItem : ImageView = v.findViewById(R.id.item_image)
+        Glide
+            .with(requireContext())
+            .load(ProductDescriptionFragmentArgs.fromBundle(requireArguments()).image)
+            .centerInside()
+            .into(imgProductItem)
         nombre_producto.text = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).title
         desc.text = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).description
         price.text = "$" + ProductDescriptionFragmentArgs.fromBundle(requireArguments()).price
