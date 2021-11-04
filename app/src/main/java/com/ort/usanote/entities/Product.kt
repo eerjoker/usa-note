66c933684b01ac8fa7289a80b5acbdcd3ef75d04
NicolasGDas <50321430+NicolasGDas@users.noreply.github.com>
2021-11-04 19:14:13 -0300

    Refactory de Product.kt para respetar el modelo de datos y poder firebase

--- app/src/main/java/com/ort/usanote/entities/Product.kt
+++ app/src/main/java/com/ort/usanote/entities/Product.kt
@@ -1,10 +1,32 @@
 package com.ort.usanote.entities
 
 class Product (
-    var title : String,
-    var description: String,
-    var price : Double,
-    var stock: Int,
-    var imageUrl : String
+    idProducto:String,
+    nombre : String,
+    description: String,
+    price : Double,
+    stock: Int,
+    categoria:String,
+    marca:String,
+    imageUrl : String
 ) {
+    lateinit var idProducto:String
+    lateinit var nombre : String
+    lateinit var description: String
+    var price : Double = 0.0
+    var stock: Int = 0
+    lateinit var categoria:String
+    lateinit var marca:String
+    lateinit var imageUrl : String
+    constructor():this("","","",0.0,0,"","","")
+    init {
+        this.idProducto = idProducto!!
+        this.nombre = nombre!!
+        this.description = description!!
+        this.price = price!!
+        this.stock = stock!!
+        this.categoria = categoria!!
+        this.marca = marca!!
+        this.imageUrl = imageUrl!!
+    }
 }
