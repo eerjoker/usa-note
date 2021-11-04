ebb9f70ae204483ff08544e1e68ada0d13cd3a35
NicolasGDas <50321430+NicolasGDas@users.noreply.github.com>
2021-11-04 19:09:51 -0300

    Agregado de metodo para actualizar stock a la hora de eliminar el producto para recuparar la cantidad pautada. y limpieza de hardcodeo del init

--- app/src/main/java/com/ort/usanote/entities/Cart.kt
+++ app/src/main/java/com/ort/usanote/entities/Cart.kt
@@ -1,19 +1,15 @@
 package com.ort.usanote.entities
 
 import android.content.SharedPreferences
+import android.util.Log
+import com.google.firebase.firestore.*
 
 class Cart (
     private var productItemList : MutableList<ProductItem>,
     var onChange : (Double) -> Unit
 ) {
+    private lateinit var db : FirebaseFirestore
 
-//    init {
-//        productItemList.add(ProductItem(Product("Keyboard", "Keyboard description", 200.0, url), 2))
-//        productItemList.add(ProductItem(Product("Mouse", "Mouse description", 90.0, url), 5))
-//        productItemList.add(ProductItem(Product("Microchip", "Microchip description", 170.0, url), 1))
-//        productItemList.add(ProductItem(Product("Screen", "Screen description", 900.0, url), 3))
-//        productItemList.add(ProductItem(Product("Headphones", "Headphones description", 940.0, url), 2))
-//    }
 
     fun getProductItems () : MutableList<ProductItem> {
         return productItemList
@@ -30,6 +26,16 @@ class Cart (
     }
 
     fun deleteProductItem(pos: Int) {
+        print(productItemList)
+
+        db = FirebaseFirestore.getInstance()
+        val productoActualizar = db.collection("productos").document(productItemList[pos].product.idProducto)
+        val nuevoStock = productItemList[pos].product.stock
+        print(nuevoStock)
+        productoActualizar.update("stock",nuevoStock)
+            .addOnSuccessListener { Log.d("TAG", "DocumentSnapshot successfully updated!") }
+            .addOnFailureListener { e -> Log.d("TAG", "Error updating document", e) }
+
         productItemList.removeAt(pos)
         onChange(calculateSubtotal())
     }
