33051a93e2a7be5744df3cb41c6ec01d8efd3cd4
NicolasGDas <50321430+NicolasGDas@users.noreply.github.com>
2021-11-04 19:20:45 -0300

    Agregado de actualizado de base de datos a la hora de agregar producto al carrito y agregado de nuevas variables recibidas por parametros

--- app/src/main/java/com/ort/usanote/fragments/ProductDescriptionFragment.kt
+++ app/src/main/java/com/ort/usanote/fragments/ProductDescriptionFragment.kt
@@ -2,14 +2,18 @@ package com.ort.usanote.fragments
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
+import com.google.firebase.firestore.*
 import com.ort.usanote.R
+import com.ort.usanote.activities.SearchActivity
 import com.ort.usanote.entities.Product
 import com.ort.usanote.entities.ProductItemRepository
 
@@ -23,12 +27,16 @@ class ProductDescriptionFragment : Fragment() {
     private lateinit var btnToCart:Button
     private lateinit var btnAddCart: Button
     private lateinit var itemsCarrito : ProductItemRepository
+    private lateinit var db : FirebaseFirestore
+    private var cant_imported = 0
+    private lateinit var idProducto: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         v = inflater.inflate(R.layout.product_description_fragment, container, false)
+        db = FirebaseFirestore.getInstance()
         btnToCartInit(v)
         btnAddCart(v)
         return v
@@ -47,7 +55,7 @@ class ProductDescriptionFragment : Fragment() {
     private fun btnToCartInit(v:View){
         btnToCart = v.findViewById(R.id.comprar)
         btnToCart.setOnClickListener{
-            itemsCarrito = addToCart(v)
+            itemsCarrito = (activity as SearchActivity).itemsCarrito
             val action = ProductDescriptionFragmentDirections.actionProductDescriptionFragmentToCarritoFragment(itemsCarrito)
             v.findNavController().navigate(action)
         }
@@ -55,13 +63,34 @@ class ProductDescriptionFragment : Fragment() {
 
     fun addToCart(v:View):ProductItemRepository{
         val spinner = v.findViewById<Spinner>(R.id.spinner_cantidad)
+        idProducto = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).idProducto
         val title = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).title
         val desc = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).description
         val price = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).price
         val image = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).image
         val stock = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).cantidad
+        val categoria = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).categoria
+        val marca = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).marca
         var ret = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).itemsCarrito
-        ret.addProductItem(Product(title,desc,price,stock,image), spinner.selectedItem as Int)
+        ret.addProductItem(Product(idProducto,title,desc,price,stock,categoria,marca,image), spinner.selectedItem as Int)
+        var nuevoStock = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).cantidad - spinner.selectedItem as Int
+        if (nuevoStock < 0){
+            nuevoStock = 0
+        }
+        val productoActualizar = db.collection("productos").document(idProducto)
+        productoActualizar.update("stock",nuevoStock)
+            .addOnSuccessListener { Log.d("TAG", "DocumentSnapshot successfully updated!") }
+            .addOnFailureListener { e -> Log.d("TAG", "Error updating document", e) }
+        //
+        cant_imported = nuevoStock
+        val array = arrayOfNulls<Number>(cant_imported)
+        for (i in array.indices){
+            array[i] = i +1
+        }
+        val adaptador = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item,array)
+        spinner.adapter = adaptador
+        //
+
         return ret
     }
 
@@ -72,7 +101,9 @@ class ProductDescriptionFragment : Fragment() {
         var desc = v.findViewById<TextView>(R.id.item_desc)
         var price = v.findViewById<TextView>(R.id.item_price)
         var spinner = v.findViewById<Spinner>(R.id.spinner_cantidad)
-        val cant_imported = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).cantidad
+        if (cant_imported == 0){
+            cant_imported = ProductDescriptionFragmentArgs.fromBundle(requireArguments()).cantidad
+        }
         val array = arrayOfNulls<Number>(cant_imported)
         for (i in array.indices){
             array[i] = i +1
