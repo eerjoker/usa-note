6131c2475ab7719e3458566534a1abdae5091f36
ezesalo <ezesalo@gmail.com>
2021-11-04 21:48:51 -0300

    Agregado de la seccion perfil. Falta completar editables

--- app/src/main/java/com/ort/usanote/fragments/UserFragment.kt
+++ app/src/main/java/com/ort/usanote/fragments/UserFragment.kt
@@ -6,10 +6,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.ort.usanote.R
+import com.ort.usanote.adapters.DireccionUserAdapter
+import com.ort.usanote.entities.Direccion
 import com.ort.usanote.viewModels.UserViewModel
 
 class UserFragment : Fragment() {
+
+    lateinit var v: View
+    lateinit var userTitle: TextView
+    lateinit var nombreCompleto: TextView
+    lateinit var emailUser: TextView
+    lateinit var telefonoUser: TextView
+    lateinit var btnDireccion: Button
+    lateinit var btnSecundarioDireccion: FloatingActionButton
+    lateinit var recDireccion: RecyclerView
+    lateinit var direccionUserAdapter: DireccionUserAdapter
+    private val viewModelUser: UserViewModel by viewModels()
 
     companion object {
         fun newInstance() = UserFragment()
@@ -21,7 +42,19 @@ class UserFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.user_fragment, container, false)
+        v = inflater.inflate(R.layout.user_fragment, container, false)
+
+        userTitle = v.findViewById(R.id.userTitle)
+        nombreCompleto = v.findViewById(R.id.nombreUser)
+        emailUser = v.findViewById(R.id.emailUser)
+        telefonoUser = v.findViewById(R.id.telefonoUser)
+        btnDireccion = v.findViewById(R.id.agregaDireccionbtn)
+        btnSecundarioDireccion = v.findViewById(R.id.agredarDireccionMas)
+        recDireccion = v.findViewById(R.id.recyclerView2)
+
+
+
+        return v
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -30,4 +63,41 @@ class UserFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        viewModelUser.setUser()
+        viewModelUser.getDirecciones()
+
+        viewModel.userDb.observe(viewLifecycleOwner, Observer {
+            if (it != null){
+                nombreCompleto.text = "${it.nombre} ${it.apellido}"
+                emailUser.text = it.email
+                telefonoUser.text = it.telefono
+            }
+        })
+
+        viewModel.direccionesUser.observe(viewLifecycleOwner, Observer{
+
+            if (it != null) {
+                if(it.size > 0){
+                    recDireccion.layoutManager = LinearLayoutManager(requireContext())
+                    recDireccion.setHasFixedSize(true)
+                    direccionUserAdapter = DireccionUserAdapter(it as ArrayList<Direccion>, requireContext())
+                    recDireccion.adapter = direccionUserAdapter
+                }
+            }
+        })
+
+        btnDireccion.setOnClickListener(){
+            val action = UserFragmentDirections.actionUserFragmentToDireccionFragment()
+            v.findNavController().navigate(action)
+        }
+
+        btnSecundarioDireccion. setOnClickListener(){
+            val action = UserFragmentDirections.actionUserFragmentToDireccionFragment()
+            v.findNavController().navigate(action)
+        }
+
+    }
 }
--- app/src/main/java/com/ort/usanote/viewModels/UserViewModel.kt
+++ app/src/main/java/com/ort/usanote/viewModels/UserViewModel.kt
@@ -1,7 +1,46 @@
 package com.ort.usanote.viewModels
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.firestore.ktx.toObjects
+import com.google.firebase.ktx.Firebase
+import com.ort.usanote.entities.Direccion
+import com.ort.usanote.entities.Usuario
 
 class UserViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val db = FirebaseFirestore.getInstance()
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    var direccionesUser = MutableLiveData<List<Direccion>??>()
+    var userDb = MutableLiveData<Usuario?>()
+
+    fun getDirecciones(){
+
+        db.collection("direcciones")
+            .whereEqualTo("userId", auth.uid!!)
+            .get().addOnSuccessListener{ snapshot ->
+                if(snapshot != null){
+                    direccionesUser.value =  snapshot.toObjects()
+                }
+            }
+            .addOnFailureListener(){
+                direccionesUser.value = null
+            }
+
+    }
+
+    fun setUser(){
+
+        db.collection("usuarios").document(auth.currentUser!!.uid).get().addOnSuccessListener {
+            if(it != null){
+                userDb.value = it.toObject<Usuario>()
+            }else{
+                userDb.value = null
+            }
+        }
+    }
 }
