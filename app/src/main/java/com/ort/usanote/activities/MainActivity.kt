5b8cde1c2549eba845ce9e18b563f9204cb7357e
NicolasGDas <50321430+NicolasGDas@users.noreply.github.com>
2021-11-09 17:51:02 -0300

    Agregado de searchview en los xml de drawer menu y en el MainActivity.kt

--- app/src/main/java/com/ort/usanote/activities/MainActivity.kt
+++ app/src/main/java/com/ort/usanote/activities/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.ort.usanote.activities
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.widget.SearchView
+import androidx.core.os.bundleOf
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.NavDeepLinkBuilder
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
@@ -15,13 +20,13 @@ import com.ort.usanote.R
 import com.ort.usanote.entities.Product
 import com.ort.usanote.entities.ProductItemRepository
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var drawerLayout : DrawerLayout
     private lateinit var navigationView: NavigationView
     private lateinit var navController: NavController
-    lateinit var itemsCarrito : ProductItemRepository
-    lateinit var productListActivity : MutableList<Product>
+    var itemsCarrito : ProductItemRepository = ProductItemRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +35,6 @@ class MainActivity : AppCompatActivity() {
         drawerLayout = findViewById(R.id.drawer_layout_search)
         navigationView = findViewById(R.id.navigation_view_search)
         navController = Navigation.findNavController(this,R.id.nav_host_fragment_search)
-        itemsCarrito = ProductItemRepository()
-        productListActivity = mutableListOf()
-        productListActivity.clear()
         navigationView.setupWithNavController(navController)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
     }
@@ -46,6 +48,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.not_logged_drawer_menu, menu)
+        val searchView_item = menu.findItem(R.id.action_search_offline)
+        val searchView:androidx.appcompat.widget.SearchView
+        searchView = searchView_item.actionView as androidx.appcompat.widget.SearchView
+        searchView.queryHint = "Ingrese el nombre del producto a buscar"
+        searchView.setOnQueryTextListener(object :
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // task HERE
+                val bundle = bundleOf("searchQuery" to query,
+                "categoria" to "busqueda")
+                val action = NavDeepLinkBuilder(this@MainActivity)
+                    .setGraph(R.navigation.main_navgraph)
+                    .addDestination(R.id.productosFragment,bundle)
+                    .createPendingIntent()
+                action.send()
+                return false
+            }
+
+        })
         return true
     }
 
