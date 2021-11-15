78ecf72529aa92df9fde3a89f90c866ee6a99319
altman-44 <altman.nicolas44@gmail.com>
2021-11-14 23:40:51 -0300

    Fix in MisCompras

--- app/src/main/java/com/ort/usanote/fragments/MisComprasFragment.kt
+++ app/src/main/java/com/ort/usanote/fragments/MisComprasFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.ort.usanote.R
 import com.ort.usanote.activities.MainActivity
 import com.ort.usanote.adapters.OrdenAdapter
@@ -43,14 +45,21 @@ class MisComprasFragment : Fragment() {
         recMisComprasOrdenes.setHasFixedSize(true)
         recMisComprasOrdenes.layoutManager = LinearLayoutManager(context)
 
-        val parentJob = Job()
-        val scope = CoroutineScope(Dispatchers.Default + parentJob)
-        scope.launch {
-            viewModel.getOrdenes("V6px2DSH8wOPaETZX8dtsSCQgOG3") { ordenes ->
-                recMisComprasOrdenes.adapter = OrdenAdapter(ordenes) { ordenId ->
-                    onOrdenClick(ordenId)
+        var user = (activity as MainActivity).auth.currentUser
+        if (user != null) {
+            val parentJob = Job()
+            val scope = CoroutineScope(Dispatchers.Default + parentJob)
+            scope.launch {
+                viewModel.getOrdenes(user.uid) { ordenes ->
+                    recMisComprasOrdenes.adapter = OrdenAdapter(ordenes) { ordenId ->
+                        onOrdenClick(ordenId)
+                    }
                 }
             }
+        } else {
+            Snackbar.make(v, R.string.not_logged_in, Snackbar.LENGTH_LONG)
+                .setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.alert_danger))
+                .show()
         }
     }
 
