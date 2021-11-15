e6ee75e5b99eea43d7078509a721f189f78e7b68
NicolasGDas <50321430+NicolasGDas@users.noreply.github.com>
2021-11-15 10:18:58 -0300

    Adaptado de productItem para db

--- app/src/main/java/com/ort/usanote/entities/ProductItem.kt
+++ app/src/main/java/com/ort/usanote/entities/ProductItem.kt
@@ -1,10 +1,16 @@
 package com.ort.usanote.entities
 
 class ProductItem (
-    var product : Product,
-    var quantity : Int
+    product : Product,
+    quantity : Int
 ) {
-
+    var product: Product
+    var  quantity: Int = 0
+    constructor():this(Product(),0)
+    init{
+        this.product = product
+        this.quantity = quantity
+    }
     fun calculateSubtotal() : Double {
         return this.product.price * this.quantity.toDouble()
     }
